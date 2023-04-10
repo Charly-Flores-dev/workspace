cfe00ee4123a7a4ed854736e46f73aec070e7007
Charlie Flo_Artz <charlessflo@outlook.com>
2023-04-09 23:09:50 -0600

    python practicas

--- python/db.py
+++ python/db.py
@@ -8,11 +8,36 @@ midb = mysql.connector.connect(
 )
 
 cursor = midb.cursor()
-
+#listar datos
 #cursor.execute('select * from Usuario')
-#sql = 'insert into Usuario()'
+#resultado = cursor.fetchall()
+#print(resultado)
 
-cursor.execute('show create table Usuario')
-
+cursor.execute('select * from Usuario limit 1')
 resultado = cursor.fetchall()
 print(resultado)
+
+#ver definicion de datos
+#cursor.execute('show create table Usuario')
+
+#insertar datos
+
+#sql = 'insert into Usuario(email, username, edad) values(%s, %s, %s)'
+#values = ('mirnasierra92@gmail.com', 'mi amor', 30)
+
+#actualizar datos
+#sql = 'update Usuario set email= %s where id=%s'
+#values= ('mirnasierra92@outlook.com',4)
+#cursor.execute(sql,values)
+
+#midb.commit()
+
+#print(cursor.rowcount)
+
+#eliminar datos
+#sql ='delete from Usuario where id=%s'
+#values=(4, )
+#cursor.execute(sql,values)
+#midb.commit()
+
+
